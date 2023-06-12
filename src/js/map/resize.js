fef2b0f060d43eb55e51a217d4d6a3c10b8609f4
zkr-1211 <81360055+zkr-1211@users.noreply.github.com>
2023-06-12 06:04:40 +0000

    Deploying to main from @ zkr-1211/act-map@aaccb833fa8d4a09f4c0993b44ad34cd02e8f65f 🚀

--- src/js/map/resize.js
+++
@@ -1,7 +0,0 @@
-export function onWindowResize(render) {
-  window.mapContext.camera.aspect = window.innerWidth / window.innerHeight
-  window.mapContext.camera.updateProjectionMatrix()
-  window.mapContext.controls.handleResize()
-  render()
-  window.mapContext.renderer.setSize(window.innerWidth, window.innerHeight)
-}
--- src/js/map/ruler.js
+++
@@ -1,5 +0,0 @@
-export const setRulerValue = function(top = '86%', value = '0km', duration = '700ms') {
-  document.getElementById('ruler_location').innerHTML = value
-  document.getElementById('ruler_location').style.top = top
-  document.getElementById('ruler_location').style.transition = duration
-}
